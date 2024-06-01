8a05b595cd90cf7d2eda457db6b35509abc566b2
Александр Кислинский <akislinskii@ashmanov.net>
2024-06-01 08:09:16 +0300

    Now can knock to a docker service

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,16 @@
 FROM pytorch/pytorch:2.3.0-cuda12.1-cudnn8-devel
 
+RUN apt-get update && apt-get install -y git
+
 COPY requirements.txt requirements.txt
 
 RUN pip install -r requirements.txt
+
+COPY app/ app/
+
+WORKDIR app/
+
+EXPOSE 8080
+
+# CMD [ "ls", "-l" ]
+CMD ["fastapi", "run", "main.py", "--port", "31000"]
--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ from typing import Union
 
 from fastapi import FastAPI
 
-from .models import HTTPValidationError, Request, Response
+from models import HTTPValidationError, Request, Response
 
 app = FastAPI(
     title='Assistant API',
@@ -26,4 +26,4 @@ def assist_assist_post(body: Request) -> Union[Response, HTTPValidationError]:
     """
     Assist
     """
-    pass
+    return Response(text="Hello world")
